9ff481413f197eab9c15329364b6db525cbf6820
idlespin55 <ajmal.ahmady@gmail.com>
2016-04-13 22:46:37 +0400

    First commit

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -9,17 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Properties
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
+    
+    //Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    //Actions
+    @IBAction func onPlayBtnPressed(sender: UIButton!) {
+            if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+                logoImg.hidden = true
+                playBtn.hidden = true
+                howManyTapsTxt.hidden = true
+                tapButton.hidden = false
+                tapsLbl.hidden = false
+                
+                maxTaps = Int(howManyTapsTxt.text!)!
+                currentTaps = 0
+                updateTapsLbl()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onCoinTapped(sender: UIButton!) {
+        currentTaps = currentTaps + 1
+        updateTapsLbl()
+        if isGameOver() {
+            restartGame()
+        }
     }
-
-
+    
+    func updateTapsLbl() {
+        tapsLbl.text = "\(currentTaps) Taps"
+    }
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func restartGame() {
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        logoImg.hidden = false
+        playBtn.hidden = false
+        howManyTapsTxt.hidden = false
+        tapButton.hidden = true
+        tapsLbl.hidden = true
+    }
+    
+    //This function makes the keyboard disappear when anywhere outside of the keyboard is clicked
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
-
